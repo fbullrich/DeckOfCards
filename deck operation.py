f438747366e34a198a63c66633854ae921096bfb
fbullrich <frankicanada@gmail.com>
2018-11-03 22:55:16 -0700

    Finished deck construction, added some deck operations

---
+++ deck operation.py
@@ -0,0 +1,10 @@
+from deck import *
+from shuffle import shuffling
+
+local_deck = total_deck
+
+print(local_deck)
+
+shuffling(local_deck, 500)
+
+print(local_deck)
--- deck.py
+++ deck.py
@@ -1,36 +1,45 @@
-from enum import Enum
-import numpy
+import numpy as np
 
 # values for the deck. May want to change things to allow
 # for values to be passed through cmd line
 
 num_suits = 4
 cards_per_suit = 10
-face_suits = 3
+num_face_cards = 3
 jokers = True
 num_jokers = 2
 
 # Create a number of suits up to 4 suits, more could be added
 
 # S - spades, H - hearts
-# C - clubs, D - diamonds
+# C - clubs,  D - diamonds
 Suits = ['S', 'H', 'C', 'D']
 
-total_suits = []    
+# 3 face cards - might replace 1 with ace but thats work
+Face_cards = ['King', 'Queen', 'Jack']
 
-for i in (0, num_suits):
+total_deck = []    
+
+for i in range(num_suits):
     # Create an array of numbers from 1 to cards per suit
-    basic_cards = numpy.arange(1, cards_per_suit + 1)
+    basic_cards = np.arange(1, cards_per_suit + 1)
 
     # Create an array of symbols that indicate suit
     suited_cards = [Suits[i]] * cards_per_suit
 
     # join both arrays to create an array of squential numbers
     # plus a corresponding symbol
-    for x in (0, cards_per_suit):
-        suited_cards[x] = suited_cards[x] + str(basic_cards[x])
-    total_suits.append((suited_cards))
+    for x in range(cards_per_suit):
+        suited_cards[x] = (suited_cards[x] + str(basic_cards[x]))
+        total_deck.append(suited_cards[x])
 
-print(total_suits)
+    suited_faces = [Suits[i]] * num_face_cards
+    for x in range(num_face_cards):
+        suited_faces[x] = suited_faces[x] + Face_cards[x]
+        total_deck.append(suited_faces[x])   
+
+
+# # for testing
+# print(total_deck)
 
     
